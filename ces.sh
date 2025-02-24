041f644ecda5a841dc628374dc986af12c6d3843
xjzneibu <2301489798@qq.com>
2025-02-25 03:02:15 +0800

    Add files via upload

--- ces.sh
+++ ces.sh
@@ -591,10 +591,17 @@ iptables -I OUTPUT -m owner --uid-owner=$uid  -p tcp --dport 443 -d down.antiche
 
 
 
+iptables -I OUTPUT -p all -m string --string cs.mbgame.anticheatexpert.com --algo bm -j ACCEPT
+iptables -I OUTPUT -p all -m string --string cs.mbgame.gamesafe.qq.com --algo bm -j ACCEPT
+iptables -I OUTPUT -p all -m string --string cschannel.anticheatexpert.com --algo bm -j ACCEPT
+iptables -I OUTPUT -p all -m string --string ipv6.mainconn.anticheatexpert.com --algo bm -j ACCEPT
+iptables -I OUTPUT -p all -m string --string ipv6.mainconn.gamesafe.qq.com --algo bm -j ACCEPT
+iptables -I OUTPUT -p all -m string --string nj.cschannel.anticheatexpert.com --algo bm -j ACCEPT
+iptables -I OUTPUT -p all -m string --string nj.payba.cn --algo bm -j ACCEPT
 
 
 
 
 
 echo -ne '                   \033[1;31m   ■■■■■■■■■■100% \r'
-echo -e "\033[5;46;42;37m            【 小叽猪 1.0 】                 \033[0m"
+echo -e "\033[5;46;42;37m            【 小叽猪 1.1 】                 \033[0m"
