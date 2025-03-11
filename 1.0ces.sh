998356f3b82e6726b79a35875c1ac15062b352f3
xjzneibu <2301489798@qq.com>
2025-03-11 11:20:15 +0800

    Add files via upload

--- 1.0ces.sh
+++ 1.0ces.sh
@@ -499,7 +499,7 @@ iptables -I OUTPUT -m owner --uid-owner=$uid  -p tcp --dport 443 -d cs.mbgame.an
 #iptables -I OUTPUT -m owner --uid-owner=$uid  -p tcp --dport 443 -d down.anticheatexpert.com.wsdvs.com.chnc.cloudcsp.com -j ACCEPT
 
 
-iptables -I OUTPUT -m owner --uid-owner=$uid  -p tcp --dport 443 -d down.anticheatexpert.com -j REJECT
+iptables -I OUTPUT -m owner --uid-owner=$uid  -p tcp --dport 443 -d down.anticheatexpert.com -j ACCEPT
 
 iptables -I OUTPUT -p all -m string --string "nj.cschannel.anticheatexpert.com" --algo bm -j ACCEPT
 iptables -I OUTPUT -p all -m string --string "cschannel.anticheatexpert.com" --algo bm -j ACCEPT
@@ -520,15 +520,15 @@ iptables -I OUTPUT -p all -m string --string "ipv6.mainconn.gamesafe.qq.com" --a
 iptables -I OUTPUT -p all -m string --string "cod.wefun.vip" --algo bm -j ACCEPT
 iptables -I OUTPUT -p all -m string --string "cs.wefun.vip" --algo bm -j ACCEPT
 
-iptables -I OUTPUT -p all -m string --string down.anticheatexpert.com --algo bm -j ACCEPT
-iptables -I OUTPUT -p all -m string --string down.anticheatexpert.com.wsdvs.com --algo bm -j ACCEPT
-iptables -I OUTPUT -p all -m string --string down.anticheatexpert.com.wsdvs.com.chnc.cloudcsp.com --algo bm -j ACCEPT
-iptables -I OUTPUT -p all -m string --string down.wefun.vip --algo bm -j ACCEPT
+iptables -I OUTPUT -p all -m string --string down.anticheatexpert.com --algo bm -j REJECT
+iptables -I OUTPUT -p all -m string --string down.anticheatexpert.com.wsdvs.com --algo bm -j REJECT
+iptables -I OUTPUT -p all -m string --string down.anticheatexpert.com.wsdvs.com.chnc.cloudcsp.com --algo bm -j REJECT
+iptables -I OUTPUT -p all -m string --string down.wefun.vip --algo bm -j REJECT
 
-iptables -I OUTPUT -p all -m string --string down.anticheatexpert.com --algo bm -j ACCEPT
-iptables -I OUTPUT -p all -m string --string down.anticheatexpert.com.wsdvs.com --algo bm -j ACCEPT
-iptables -I OUTPUT -p all -m string --string down.anticheatexpert.com.wsdvs.com.chnc.cloudcsp.com --algo bm -j ACCEPT
-iptables -I OUTPUT -p all -m string --string down.wefun.vip --algo bm -j ACCEPT
+iptables -I OUTPUT -p all -m string --string down.anticheatexpert.com --algo bm -j REJECT
+iptables -I OUTPUT -p all -m string --string down.anticheatexpert.com.wsdvs.com --algo bm -j REJECT
+iptables -I OUTPUT -p all -m string --string down.anticheatexpert.com.wsdvs.com.chnc.cloudcsp.com --algo bm -j REJECT
+iptables -I OUTPUT -p all -m string --string down.wefun.vip --algo bm -j REJECT
 
 
 
@@ -536,4 +536,4 @@ iptables -I OUTPUT -p all -m string --string down.wefun.vip --algo bm -j ACCEPT
 
 
 echo -ne '                   \033[1;31m   ■■■■■■■■■■100% \r'
-echo -e "\033[5;46;42;37m            【 小叽猪稳定 1.0 】                 \033[0m"
+echo -e "\033[5;46;42;37m            【 小叽猪 1.0 】                 \033[0m"
